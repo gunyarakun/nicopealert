9b04e5cf45b5ad81960cf9baaa6f721d81bc0d44
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2009-05-06 10:50:45 +0900

    added fetch_live_detail_from_live_id().

--- nicolive.py
+++ nicolive.py
@@ -12,21 +12,44 @@ class NicoLive:
   WATCH_URL_REGEX = re.compile(r'(http://live.nicovideo.jp)?/watch/lv(\d+)')
   
   def __init__(self):
-    pass
+    self.opener = urllib2.build_opener()
 
   def fetch_lives(self):
-    id_set = set()
+    liveid_set = set()
     for tab in self.TABS:
       url = 'http://live.nicovideo.jp/recent?tab=%s&p=1' % tab
-      self.fetch_live_ids_from_html(url, id_set)
-    print id_set
+      self.fetch_live_ids_from_html(url, liveid_set)
+    live_list = []
+    for live_id in sorted(list(liveid_set)):
+      live_detail = self.fetch_live_detail_from_live_id(live_id)
+      live_list.append(live_detail)
+    print live_list
 
   def fetch_live_ids_from_html(self, url, id_set):
-    opener = urllib2.build_opener()
-    html = opener.open(url).read()
+    html = self.opener.open(url).read()
     soup = BeautifulSoup(html)
     
-    for a in soup.findAll('a', href=self.WATCH_URL_REGEX):
+    for a in soup.findAll('a', href = self.WATCH_URL_REGEX):
       m = self.WATCH_URL_REGEX.match(a['href'])
       if m:
         id_set.add(int(m.group(2)))
+
+  def fetch_live_detail_from_live_id(self, live_id):
+    url = 'http://live.nicovideo.jp/watch/lv%d' % live_id
+    html = self.opener.open(url).read()
+    soup = BeautifulSoup(html)
+    
+    tmdl = soup.find('div', {'class': 'tmdl'})
+    # tmdlの中は、詳細コメント・主・コミュニティ・注意書きの順で<p>が入ってる
+    desct, nusit, comt, txt10 = tmdl.findAll('p')
+    desc = desct.contents[0]
+    nusi = nusit.find('span', {'class': 'nicopedia'}).contents[0]
+    com_url = comt.a['href']
+    com_id = com_url[com_url.rindex('/') + 1:]
+    com_text = comt.a.contents[0]
+
+    return {'live_id': live_id,
+            'desc': desc,
+            'nusi': nusi,
+            'com_id': com_id,
+            'com_text': com_text}
