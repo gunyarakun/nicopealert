536b1006b61b7f3bc5393579b0bc1071200dd433
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2009-05-23 10:32:08 +0900

    UI revised.

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'nicopealert.ui'
 #
-# Created: Fri May 22 09:36:21 2009
+# Created: Sat May 23 10:32:04 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -34,8 +34,17 @@ class Ui_MainWindow(object):
     self.gridLayout_2.setObjectName("gridLayout_2")
     self.horizontalLayout = QtGui.QHBoxLayout()
     self.horizontalLayout.setObjectName("horizontalLayout")
+    self.label = QtGui.QLabel(self.tab)
+    self.label.setObjectName("label")
+    self.horizontalLayout.addWidget(self.label)
+    self.dicKeywordLineEdit = QtGui.QLineEdit(self.tab)
+    self.dicKeywordLineEdit.setObjectName("dicKeywordLineEdit")
+    self.horizontalLayout.addWidget(self.dicKeywordLineEdit)
     spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
     self.horizontalLayout.addItem(spacerItem)
+    self.dicWatchListCheckBox = QtGui.QCheckBox(self.tab)
+    self.dicWatchListCheckBox.setObjectName("dicWatchListCheckBox")
+    self.horizontalLayout.addWidget(self.dicWatchListCheckBox)
     self.dicFilterPushButton = QtGui.QPushButton(self.tab)
     sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
     sizePolicy.setHorizontalStretch(0)
@@ -57,8 +66,17 @@ class Ui_MainWindow(object):
     self.gridLayout_3.setObjectName("gridLayout_3")
     self.horizontalLayout_2 = QtGui.QHBoxLayout()
     self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+    self.label_2 = QtGui.QLabel(self.tab_2)
+    self.label_2.setObjectName("label_2")
+    self.horizontalLayout_2.addWidget(self.label_2)
+    self.liveKeywordLineEdit = QtGui.QLineEdit(self.tab_2)
+    self.liveKeywordLineEdit.setObjectName("liveKeywordLineEdit")
+    self.horizontalLayout_2.addWidget(self.liveKeywordLineEdit)
     spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
     self.horizontalLayout_2.addItem(spacerItem1)
+    self.liveCommunityCheckBox = QtGui.QCheckBox(self.tab_2)
+    self.liveCommunityCheckBox.setObjectName("liveCommunityCheckBox")
+    self.horizontalLayout_2.addWidget(self.liveCommunityCheckBox)
     self.liveFilterPushButton = QtGui.QPushButton(self.tab_2)
     sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
     sizePolicy.setHorizontalStretch(0)
@@ -72,7 +90,9 @@ class Ui_MainWindow(object):
     self.liveTreeView = QtGui.QTreeView(self.tab_2)
     self.liveTreeView.setObjectName("liveTreeView")
     self.gridLayout_3.addWidget(self.liveTreeView, 0, 0, 1, 1)
-    self.tabWidget.addTab(self.tab_2, "")
+    icon1 = QtGui.QIcon()
+    icon1.addPixmap(QtGui.QPixmap("live.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+    self.tabWidget.addTab(self.tab_2, icon1, "")
     self.tab_3 = QtGui.QWidget()
     self.tab_3.setObjectName("tab_3")
     self.gridLayout_4 = QtGui.QGridLayout(self.tab_3)
@@ -116,7 +136,7 @@ class Ui_MainWindow(object):
     self.communityTreeView = QtGui.QTreeView(self.tab_4)
     self.communityTreeView.setObjectName("communityTreeView")
     self.gridLayout_5.addWidget(self.communityTreeView, 0, 0, 1, 1)
-    self.tabWidget.addTab(self.tab_4, "")
+    self.tabWidget.addTab(self.tab_4, icon1, "")
     self.gridLayout.addWidget(self.tabWidget, 0, 0, 1, 1)
     MainWindow.setCentralWidget(self.centralwidget)
     self.menubar = QtGui.QMenuBar(MainWindow)
@@ -136,16 +156,20 @@ class Ui_MainWindow(object):
 
   def retranslateUi(self, MainWindow):
     MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "ニコ百アラート", None, QtGui.QApplication.UnicodeUTF8))
-    self.dicFilterPushButton.setText(QtGui.QApplication.translate("MainWindow", "フィルタする", None, QtGui.QApplication.UnicodeUTF8))
+    self.label.setText(QtGui.QApplication.translate("MainWindow", "検索キーワード", None, QtGui.QApplication.UnicodeUTF8))
+    self.dicWatchListCheckBox.setText(QtGui.QApplication.translate("MainWindow", "ウォッチリスト限定", None, QtGui.QApplication.UnicodeUTF8))
+    self.dicFilterPushButton.setText(QtGui.QApplication.translate("MainWindow", "この条件を保存", None, QtGui.QApplication.UnicodeUTF8))
     self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QtGui.QApplication.translate("MainWindow", "大百科", None, QtGui.QApplication.UnicodeUTF8))
     self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.tab), QtGui.QApplication.translate("MainWindow", "ニコニコ大百科のイベント一覧です。", None, QtGui.QApplication.UnicodeUTF8))
-    self.liveFilterPushButton.setText(QtGui.QApplication.translate("MainWindow", "フィルタする", None, QtGui.QApplication.UnicodeUTF8))
+    self.label_2.setText(QtGui.QApplication.translate("MainWindow", "検索キーワード", None, QtGui.QApplication.UnicodeUTF8))
+    self.liveCommunityCheckBox.setText(QtGui.QApplication.translate("MainWindow", "対象コミュニティ限定", None, QtGui.QApplication.UnicodeUTF8))
+    self.liveFilterPushButton.setText(QtGui.QApplication.translate("MainWindow", "この条件を保存", None, QtGui.QApplication.UnicodeUTF8))
     self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QtGui.QApplication.translate("MainWindow", "生放送", None, QtGui.QApplication.UnicodeUTF8))
     self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.tab_2), QtGui.QApplication.translate("MainWindow", "ニコニコ生放送の放送一覧です。", None, QtGui.QApplication.UnicodeUTF8))
     self.addWatchListPushButton.setText(QtGui.QApplication.translate("MainWindow", "ウォッチリストを追加する", None, QtGui.QApplication.UnicodeUTF8))
     self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), QtGui.QApplication.translate("MainWindow", "ウォッチリスト", None, QtGui.QApplication.UnicodeUTF8))
     self.addCommunityPushButton.setText(QtGui.QApplication.translate("MainWindow", "コミュニティを追加", None, QtGui.QApplication.UnicodeUTF8))
-    self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QtGui.QApplication.translate("MainWindow", "コミュニティ", None, QtGui.QApplication.UnicodeUTF8))
+    self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), QtGui.QApplication.translate("MainWindow", "対象コミュニティ", None, QtGui.QApplication.UnicodeUTF8))
     self.menu.setTitle(QtGui.QApplication.translate("MainWindow", "ファイル", None, QtGui.QApplication.UnicodeUTF8))
 
 
